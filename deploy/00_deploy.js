aed91db5f8e8e3995930cbaab5a647c6d94acc32
Xiangan He <xiangan9@gmail.com>
2023-02-26 17:33:03 -0500

    dealclient deployment wip

--- deploy/00_deploy.js
+++ deploy/00_deploy.js
@@ -10,16 +10,8 @@ const wallet = new ethers.Wallet(private_key, ethers.provider)
 module.exports = async ({ deployments }) => {
     console.log("Wallet Ethereum Address:", wallet.address)
     const chainId = network.config.chainId
-    const tokensToBeMinted = networkConfig[chainId]["tokensToBeMinted"]
 
-    //deploy Simplecoin
-    const CBORParse = await ethers.getContractFactory('CBORParse', wallet);
-    console.log('Deploying CBORParse...');
-    const cborparse = await CBORParse.deploy(tokensToBeMinted);
-    await cborparse.deployed()
-    console.log('CBORParse deployed to:', cborparse.address);
-
-    //deploy FilecoinMarketConsumer
+    //deploy DealClient
     const DealClient = await ethers.getContractFactory('DealClient', wallet);
     console.log('Deploying DealClient...');
     const dealclient = await DealClient.deploy();
