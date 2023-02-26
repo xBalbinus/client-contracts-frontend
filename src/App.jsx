974aa13f657772993239403c6206e593fca19cc5
Xiangan He <xiangan9@gmail.com>
2023-02-26 18:36:07 -0500

    fresh frontend with button and box

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CarFileInput from './CarFileInput';
+
+function App() {
+  return (
+    <div className="App">
+      <CarFileInput />
+    </div>
+  );
+}
+
+export default App;
