055cda159bdb75938276cdb451b6bd6c35f27e2d
Xiangan He <xiangan9@gmail.com>
2023-03-01 09:03:57 -0500

    frontend

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from 'react';
 import contract from '../contracts/DealClient.json';
 import { ethers } from 'ethers';
 
-const contractAddress = "0x4d0fB4EB0874d49AA36b5FCDb8321599817c723F";
+const contractAddress = "0xab76119E5B3863c5d297693384777d5231E0Aeb2";
 const contractABI = contract.abi;
 
 function Inputs() {
   const [commP, setCommP] = useState('');
   const [carLink, setCarLink] = useState('');
   const [pieceSize, setPieceSize] = useState('');
+  const [startEpoch, setStartEpoch] = useState('');
+  const [endEpoch, setEndEpoch] = useState('');
 
   const handleChangeCommP = (event) => {
     setCommP(event.target.value);
@@ -22,13 +24,63 @@ function Inputs() {
     setPieceSize(event.target.value);
   }
 
-  const handleSubmit = (event) => {
+  const handleChangeStartEpoch = (event) => {
+    setStartEpoch(event.target.value);
+  }
+
+  const handleChangeEndEpoch = (event) => {
+    setEndEpoch(event.target.value);
+  }
+
+  const handleSubmit = async (event) => {
     // This will be handling deal proposal submission sometime in the future.
     event.preventDefault();
     // do something with the carLink value, like send it to a backend API
     console.log(commP);
     console.log(carLink);
     console.log(pieceSize);
+
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.BrowserProvider(ethereum);
+        const signer = provider.getSigner();
+        const dealClient = new ethers.Contract(contractAddress, contractABI, signer);
+        const extraParamsV1 = [
+          carLink,
+          1175,
+          false, // taskArgs.skipIpniAnnounce,
+          false // taskArgs.removeUnsealedCopy
+        ]
+        const DealRequestStruct = [
+          commP, //cidHex
+          pieceSize, //taskArgs.pieceSize,
+          false, //taskArgs.verifiedDeal,
+          "bafk2bzacec3jst4tkh424chatp273o6rxvipfg54kphd56gaxobpcdtr2sgco", //taskArgs.label,
+          startEpoch,
+          endEpoch,
+          0, // taskArgs.storagePricePerEpoch,
+          0, // taskArgs.providerCollateral,
+          0, // taskArgs.clientCollateral, 
+          1, //taskArgs.extraParamsVersion,
+          extraParamsV1,
+        ]
+        // console.log(await provider.getBalance("0x42c930a33280a7218bc924732d67dd84d6247af4"));
+        console.log(dealClient.interface);
+        const transaction = await dealClient.makeDealProposal(DealRequestStruct);
+        console.log("Proposing deal...", );
+        const receipt = await transaction.wait();
+        console.log(receipt);
+        
+        console.log("Deal proposed!");
+      }
+      else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    }
+    catch (error) {
+      console.log(error);
+    }
   }
 
   const checkWalletIsConnected = () => {
@@ -63,31 +115,6 @@ function Inputs() {
       .catch(err => console.log(err));
   }
 
-  const makeDealProposalHandler = async () => {
-    try {
-      const { ethereum } = window;
-      if (ethereum) {
-        const provider = new ethers.BrowserProvider(ethereum);
-        const signer = provider.getSigner();
-        const dealClient = new ethers.Contract(contractAddress, contractABI, signer);
-        // console.log(await provider.getBalance("0x42c930a33280a7218bc924732d67dd84d6247af4"));
-        console.log(dealClient.interface);
-        const transaction = await dealClient.makeDealProposal("0x42c930a33280a7218bc924732d67dd84d6247af4");
-        console.log("Proposing deal...", );
-        const receipt = await transaction.wait();
-        console.log(receipt);
-        
-        console.log("Deal proposed!");
-      }
-      else {
-        console.log("Ethereum object doesn't exist!");
-      }
-    }
-    catch (error) {
-      console.log(error);
-    }
-  }
-
   const connectWalletButton = () => {
     return (
       <button onClick={connectWalletHandler} className='cta-button connect-wallet-button'>
@@ -118,7 +145,15 @@ function Inputs() {
         Piece Size:
         <input type="text" value={pieceSize} onChange={handleChangePieceSize} />
       </div>
-      <button type="submit" onClick={makeDealProposalHandler} style={{ display: 'block' , width: '50%', margin: 'auto' }}>Submit</button>
+      <div style = {{ display: 'block' , width: '50%', margin: 'auto' }}>
+        Start Epoch:
+        <input type="text" value={startEpoch} onChange={handleChangeStartEpoch} />
+      </div>
+      <div style = {{ display: 'block' , width: '50%', margin: 'auto' }}>
+        End Epoch:
+        <input type="text" value={endEpoch} onChange={handleChangeEndEpoch} />
+      </div>
+      <button type="submit" style={{ display: 'block' , width: '50%', margin: 'auto' }}>Submit</button>
     </form>
     
   );
